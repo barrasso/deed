77d8317d1d851598bbd265284bdd8d179df75088
Mark Barrasso <4982406+barrasso@users.noreply.github.com>
2021-09-05 16:55:06 -0400

    initial commit

---
+++ test/deed.ts
@@ -0,0 +1,41 @@
+import { ethers} from "hardhat";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { Deed__factory, Deed } from "../typechain";
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe("Deed", () => {
+  let deed: Deed;
+  const numberToMint = 64;
+
+  beforeEach(async () => {
+    // 1
+    const signers = await ethers.getSigners();
+
+    // 2
+    const deedFactory = (await ethers.getContractFactory(
+      "Deed",
+      signers[0]
+    )) as Deed__factory;
+    deed = await deedFactory.deploy();
+    await deed.deployed();
+
+
+    // Mint some deeds.
+    await deed.testClaim();
+  });
+
+  // 4
+  describe("tokenURI()", async () => {
+    it("should print token URI string", async () => {
+      let i = 0;
+      while(i != numberToMint){
+        const uri = await deed.tokenURI(i);
+        i++;
+      }
+    });
+  });
+
+});
